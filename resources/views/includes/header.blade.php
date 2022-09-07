ca5211b208faa8dc12a3638d7aa27eb166f1862c
ErJump <fedelijunior@gmail.com>
2022-09-07 14:20:07 +0200

    feat: :sparkles: Styles Header

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,21 @@
 <header>
-    <h1>Header</h1>
+    <nav class="navbar navbar-expand-lg bg-white">
+        <div class="container-lg d-flex ">
+            <img class="" src="/images/dc-logo.png" alt="logo">
+            <div class=" justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav justify-self-end">
+                    <a class="nav-link active px-3" href="#">Characters</a>
+                    <a class="nav-link active px-3" href="#">Comics</a>
+                    <a class="nav-link active px-3" href="#">Movies</a>
+                    <a class="nav-link active px-3" href="#">TV</a>
+                    <a class="nav-link active px-3" href="#">Games</a>
+                    <a class="nav-link active px-3" href="#">Collectibles</a>
+                    <a class="nav-link active px-3" href="#">Videos</a>
+                    <a class="nav-link active px-3" href="#">Fans</a>
+                    <a class="nav-link active px-3" href="#">News</a>
+                    <a class="nav-link active px-3" href="#">Shop</a>    
+                </div>
+            </div>
+        </div>
+    </nav>
 </header>

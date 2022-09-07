50892c9456deb9927d957fb0036663bd176bfd13
ErJump <fedelijunior@gmail.com>
2022-09-07 14:01:20 +0200

    feat: :sparkles: Adds variables.scss + imports

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
-
+@section('title', 'Home')
 @section('content')
     <section>
         <h1>Main-Content</h1>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DC Comics |</title>
+    <title>DC Comics | @yield('title')</title>
+    <link href="{{asset('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
     @include('includes.header')
